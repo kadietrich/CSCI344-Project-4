8cfae4b3ea101e02343e8d19f0055a46264b3c77
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-04-01 11:59:49 -0400

    Initial commit. Copy code over from tutorial files.

---
+++ app.js
@@ -0,0 +1,58 @@
+// We need to 'require' the following modules                                                                                                                    
+var express = require("express"),
+    http = require("http"),
+    path = require("path"),
+    redisClient = require ("redis").createClient(),
+    app = express();
+    
+var happyTerms = ['happy', 'excited', 'fun', 'glad', 'good'];
+var sadTerms = ['sad', 'depressed', 'crying', 'bad'];
+// This is our basic configuration                                                                                                                     
+app.configure(function () {
+    // Define our static file directory, it will be 'public'                                                                                           
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+// Create the http server and get it to listen on the specified port 3000                                                                                                                   
+http.createServer(app).listen(3000, function(){
+    console.log("Express server listening on port 3000");
+});
+
+app.get("/", function (req, res) {
+    //send "Hello World" to the client as html
+    res.send("Hello World!");
+});
+app.get("/counts.json", function	(req, res) {
+    redisClient.get("awesome", function	(error, awesomeCount) {
+        if (error !== null) {
+            // handle error here                                                                                                                       
+            console.log("ERROR: " + error);
+        } else {
+            var jsonObject = {
+                "awesome":awesomeCount
+            };
+            // use res.json to return JSON objects instead of strings
+            res.json(jsonObject);
+        }
+    });
+});
+app.get("/happycounts.json", function	(req, res) {
+    redisClient.mget(happyTerms, function	(error, happyCount) {
+        var i = 0;
+        if (error !== null) {
+            // handle error here                                                                                                                       
+            console.log("ERROR: " + error);
+        } else {
+            happyCount.forEach( function(elt) {
+                var stuff = happyTerms[i];
+                var jsonObject = {
+                    stuff:elt
+                };
+            i++;  
+            });
+            
+            // use res.json to return JSON objects instead of strings
+            res.json(jsonObject);
+        }
+    });
+});
