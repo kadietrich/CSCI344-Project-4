f1b87457e08407b9fbbca2c3ff85ac4662413131
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-04-04 13:25:14 -0400

    Rearrange items and add headers in preparation for adding styles.

--- public/javascripts/client-app.js
+++ public/javascripts/client-app.js
@@ -1,20 +1,29 @@
 var main = function () {
-    $.getJSON("/happyTotal.json", function (object) {
-        var happyTotal = parseInt(object.count);
-        $("#happy").append("<p>The total happy count is: " + object.count + "</p>");
-        
+    $.getJSON("/happyTotal.json", function (result) {
+        var happyTotal = parseInt(result.count);
+        $("#happy").append("<p>The total happy count is: " + result.count + "</p>");
         $.getJSON("/sadTotal.json", function (result) {
             var sadTotal = parseInt(result.count);
             $("#sad").append("<p>The sad count is: " + result.count + "</p>");
             if ( happyTotal > sadTotal) {
-                $("#overall").append("<p>Today is a happy day.</p>");
+                $("#overall").append("<h2>Today is a happy day.</h2>");
             } else if (sadTotal > happyTotal) {
-                $("#overall").append("<p>Today is a sad day.</p>");
+                $("#overall").append("<h2>Today is a sad day.</h2>");
             } else {
-                $("#overall").append("<p>Today isn't a happy or sad day.</p>");
+                $("#overall").append("<h2>Today isn't a happy or sad day.</h2>");
             }
         });
-    }); 
+    });
+    $.getJSON("/happyCounts.json", function (object) {
+        object.forEach( function (word) {
+            $("#happyList").append("/" + word.key + "/")
+        });
+    });
+    $.getJSON("/sadCounts.json", function (object) {
+        object.forEach( function (word) {
+            $("#sadList").append("/" + word.key + "/")
+        });
+    });
 };
 
 $(document).ready(main);
